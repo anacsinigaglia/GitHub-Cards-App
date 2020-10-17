683ceff76ccb81d6076bce0d7fd335dd26e85ab3
Ana Caroline Sinigaglia <ana.sinigaglia@iteris.com.br>
2020-10-17 14:21:47 -0300

    Palavra async antes da function

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,7 +5,7 @@ class Form extends React.Component {
 //é uma referência pra chamar o input (nesse caso, é uma string vazia),
 //usando isso no value do input, conseguimos controlar o valor desse input e tornar o react ciente do que se passa nele
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault(); //só pra não dar refresh na página quando dermos submit
         const resp = await axios.get(`https://api.github.com/user/${this.state.userName}`);
         this.props.onSubmit(resp.data);
